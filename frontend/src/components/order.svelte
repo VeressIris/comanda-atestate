78a27e78d362801203e04332ff79c8b23288fcd3
VeressIris <irisveress@gmail.com>
2024-06-22 23:39:56 +0300

    styling changes + changing bg color based on completion of project

--- frontend/src/components/order.svelte
+++ frontend/src/components/order.svelte
@@ -2,20 +2,28 @@
   export let name: string;
   export let technologies: string[];
   export let complexity: number;
+  let completed = false;
 </script>
 
 <div
-  class="bg-success-content
- rounded-md p-4"
+  class="rounded-md p-4"
+  class:bg-neutral-content={completed}
+  class:bg-success-content={!completed}
 >
-  <h1 class="mb-3">{name}</h1>
+  <h1 class="mb-4">{name}</h1>
   <p><span class="font-medium">Tehnologii:</span> {technologies}</p>
   <p><span class="font-medium">Complexitate:</span> {complexity}/5</p>
-  <div class="form-control">
-    <label class="label cursor-pointer justify-end pb-0">
-      <span class="label text-lg mr-2 p-0">Terminat: </span>
-      <input type="checkbox" class="checkbox checkbox-primary" />
-    </label>
+  <div class="flex flex-row-reverse items-center justify-between">
+    <div class="form-control">
+      <label class="label cursor-pointer justify-end p-0">
+        <span class="label text-lg mr-2">Terminat: </span>
+        <input
+          bind:checked={completed}
+          type="checkbox"
+          class="checkbox checkbox-primary"
+        />
+      </label>
+    </div>
+    <a href="/dashboard/{name}" class="link link-primary">Vezi detalii</a>
   </div>
-  <a href="/dashboard/{name}" class="link link-primary">Vezi detalii</a>
 </div>
