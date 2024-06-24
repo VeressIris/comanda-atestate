9c63e98c4a5de2047fcdcfc6bd8f2bd3cce54f86
VeressIris <irisveress@gmail.com>
2024-06-24 17:50:26 +0300

    added completed icon

--- src/routes/dashboard/[name]/+page.svelte
+++ src/routes/dashboard/[name]/+page.svelte
@@ -21,7 +21,22 @@
   }
 </script>
 
-<h1 class="mb-4">Proiect pentru <i>{data.order.name}</i></h1>
+<div class="flex items-center mb-4">
+  <h1 class="mr-3.5">Proiect pentru <i>{data.order.name}</i></h1>
+  {#if data.order.completed}
+    <svg
+      class="mt-0.5"
+      xmlns="http://www.w3.org/2000/svg"
+      height="42px"
+      width="42px"
+      viewBox="0 -960 960 960"
+      fill="#823290"
+      ><path
+        d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q65 0 123 19t107 53l-58 59q-38-24-81-37.5T480-800q-133 0-226.5 93.5T160-480q0 133 93.5 226.5T480-160q133 0 226.5-93.5T800-480q0-18-2-36t-6-35l65-65q11 32 17 66t6 70q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm-56-216L254-466l56-56 114 114 400-401 56 56-456 457Z"
+      /></svg
+    >
+  {/if}
+</div>
 <h3 class="mb-2">Număr de contact: {data.order.phoneNumber}</h3>
 <h3 class="mb-2">Tehnologii sugerate: {data.order.technologies}</h3>
 <h3 class="mb-2">
