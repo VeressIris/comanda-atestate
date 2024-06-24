02bc8d8f3b3c1acf75ff172acbcbd56c86fa73ae
VeressIris <irisveress@gmail.com>
2024-06-24 18:16:22 +0300

    added login form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,9 @@
         class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-success-content rounded-box w-52"
       >
         <li><a href="/dashboard">Dashboard</a></li>
-        <li><button>Login/logout</button></li>
+        <li>
+          <button on:click={() => loginModal.showModal()}>Login/logout</button>
+        </li>
       </ul>
     </div>
   </div>
@@ -55,3 +57,47 @@
 <div class="mx-32">
   <slot></slot>
 </div>
+<dialog id="loginModal" class="modal">
+  <div class="modal-box">
+    <form method="dialog">
+      <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+        >✕</button
+      >
+    </form>
+    <h3 class="text-lg font-bold">Login:</h3>
+    <div class="flex flex-col items-center">
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="h-4 w-4 opacity-70"
+        >
+          <path
+            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z"
+          />
+        </svg>
+        <input type="text" class="grow" placeholder="Username" />
+      </label>
+      <label class="input input-bordered flex items-center gap-2 my-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          class="h-4 w-4 opacity-70"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+            clip-rule="evenodd"
+          />
+        </svg>
+        <input type="password" class="grow" placeholder="xxxx" />
+      </label>
+      <button
+        class="btn btn-outline btn-primary mt-2 min-h-5 h-10 px-5 text-base"
+        >Login</button
+      >
+    </div>
+  </div>
+</dialog>
